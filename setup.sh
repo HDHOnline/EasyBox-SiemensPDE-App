eee2f987af7dea81e7b62e91fb5531fe0dac0184
vi.mys <gnupp@gmx.net>
2022-11-19 18:25:44 +0100

    small, but important, changes

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 # The whole repos was cloned to /home/pi/EasyBox-SiemensPDE-App by JFrog
 
 cd /etc/init.d/
-sudo cp /home/pi/EasyBox-SiemensPDE-App/setup/EasyBox-SiemensPDE autostart
+sudo cp /home/pi/EasyBox-SiemensPDE-App/setup/EasyBox-SiemensPDE SiemensPDE
 sudo chmod 755 /etc/init.d/EasyBox-SiemensPDE
 sudo update-rc.d EasyBox-SiemensPDE defaults
 # to delete EasyBox-SiemensPDE script: sudo update-rc.d -f EasyBox-SiemensPDE remove
@@ -20,7 +20,7 @@ sudo chown pi:pi /home/logs
 cd /home/pi/
 sudo mkdir deploy
 cd /home/pi/deploy/
-sudo cp /home/pi/EasyBox-SiemensPDE-App/deploy/SiemensPDE.jar SiemensPDE.jar
+sudo cp /home/pi/EasyBox-SiemensPDE-App/out/artifacts/SiemensPDE_jar/SiemensPDE.jar SiemensPDE.jar
 sudo chmod 777 SiemensPDE.jar
 sudo chown pi:pi SiemensPDE.jar
 
